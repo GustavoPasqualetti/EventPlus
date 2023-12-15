ce3c1a00a3d1e1e8ab27a756af24f05699dfdeda
Gustavo Pasqualetti <49802004863@info.local>
2023-12-15 08:37:17 -0300

    implementacao da IA

---
+++ webapi.event+/Controllers/ComentariosController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.CognitiveServices.ContentModerator;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using webapi.event_.Domains;
+using webapi.event_.Interfaces;
+using webapi.event_.Repositories;
+
+namespace webapi.event_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ComentariosController : ControllerBase
+    {
+        ComentariosEventoRepository comentario = new ComentariosEventoRepository();
+
+        private readonly ContentModeratorClient _contentModeratorClient;
+
+        public ComentariosController(ContentModeratorClient contentModeratorClient)
+        {
+            _contentModeratorClient = contentModeratorClient;
+        }
+
+        [HttpPost("ComentarioIA")]
+        public async Task<IActionResult> PostIa(ComentariosEvento novoComentario)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(novoComentario.Descricao))
+                {
+                    return BadRequest("A descricao do comentario nao pode estar vazio ou nulo");
+                }
+
+                using var stream = new MemoryStream(Encoding.UTF8.GetBytes(novoComentario.Descricao));
+
+                var moderationResult = await _contentModeratorClient.TextModeration
+                    .ScreenTextAsync("text/plain", stream, "por", false, false, null, true);
+
+                if(moderationResult.Terms != null)
+                {
+                    novoComentario.Exibe = false;
+
+                    comentario.Cadastrar(novoComentario);
+                }
+                else
+                {
+                    novoComentario.Exibe = true;
+
+                    comentario.Cadastrar( novoComentario);
+
+                    
+                }
+                   return StatusCode(201, novoComentario);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Get(Guid id)
+        {
+            try
+            {
+                return Ok(comentario.Listar(id));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("BuscarPorIdUsuario")]
+        public IActionResult GetByIdUser(Guid idUsuario, Guid idEvento)
+        {
+            try
+            {
+                return Ok(comentario.BuscarPorIdUsuario(idUsuario, idEvento));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(ComentariosEvento novoComentario)
+        {
+            try
+            {
+                comentario.Cadastrar(novoComentario);
+
+                return StatusCode(201, novoComentario);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                comentario.Deletar(id);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("ListarSomenteExibe")]
+        public IActionResult GetShow(Guid id)
+        {
+            try
+            {
+                return Ok(comentario.ListarSomenteExibe(id));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ webapi.event+/Interfaces/IEventoRepository.cs
@@ -0,0 +1,16 @@
+﻿using webapi.event_.Domains;
+
+namespace webapi.event_.Interfaces
+{
+    public interface IEventoRepository
+    {
+        void Cadastrar(Evento evento);
+        void Deletar(Guid id);
+        List<Evento> Listar();
+
+        List<Evento> ListarAnteriores();
+        List<Evento> ListarProximos();
+        Evento BuscarPorId(Guid id);
+        void Atualizar(Guid id, Evento evento);
+    }
+}
