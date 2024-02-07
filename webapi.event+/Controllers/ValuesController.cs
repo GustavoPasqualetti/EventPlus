8930f54f1f34402e4c82cceec5c18cdd1cc32865
Gustavo Pasqualetti <gustavopasqualetti@gmail.com>
2024-02-07 17:36:09 -0300

    projeto EventPlus ReactJS

---
+++ webapi.event+/Controllers/ValuesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.event_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
